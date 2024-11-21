e9e3d97a391ab229be8ce13504b1feb257bb5e55
mlroc <uravgpcuser@gmail.com>
2024-11-21 15:58:17 -0500

    Initial Commit

---
+++ ARCampusTests/ARCampusTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARCampusTests.swift
+//  ARCampusTests
+//
+//  Created by Michael Liu on 11/21/24.
+//
+
+import Testing
+@testable import ARCampus
+
+struct ARCampusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
